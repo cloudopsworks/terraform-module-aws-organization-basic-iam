a4a2fa028893afcc4ac61f3c7f009221c225356e
Cristian Beraha <cristian@cloudops.works>
2025-03-27 12:43:40 -0300

    Add IAM policy for Backup admin role
    Introduced a new policy to grant full access to AWS Backup services. This includes creating and associating the policy with the existing Terraform access role.

---
+++ modules/terraform-role/policy-backup.tf
@@ -0,0 +1,21 @@
+# SES Admin Policy
+data "aws_iam_policy_document" "tf_backup_admin" {
+  version = "2012-10-17"
+  statement {
+    sid = "AllowRead"
+    actions = [
+      "backup:*",
+    ]
+
+    resources = [
+      "*",
+    ]
+  }
+}
+
+resource "aws_iam_role_policy" "terraform_access_backup_admin" {
+  name   = "BackupAdmin"
+  role   = aws_iam_role.terraform_access.name
+  policy = data.aws_iam_policy_document.tf_backup_admin.json
+}
+
