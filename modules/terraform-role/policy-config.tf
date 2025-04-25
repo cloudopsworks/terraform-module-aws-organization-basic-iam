79707fc231bb20b20df0ae767fd6e127f2743783
Cristian Beraha <cristian@cloudops.works>
2025-04-25 12:16:12 -0300

    Update IAM role policies to reference correct feature flags
    Replaced `var.settings.awsbackup` with `var.settings.awsconfig` and `var.settings.security_hub` in respective IAM policies. This ensures the policies align with the appropriate feature flags for configuration and Security Hub administration.

--- modules/terraform-role/policy-config.tf
+++ modules/terraform-role/policy-config.tf
@@ -15,7 +15,7 @@ data "aws_iam_policy_document" "tf_config_admin" {
 }
 
 resource "aws_iam_role_policy" "terraform_access_config_admin" {
-  count  = try(var.settings.awsbackup, false) ? 1 : 0
+  count  = try(var.settings.awsconfig, false) ? 1 : 0
   name   = "ConfigAdmin"
   role   = aws_iam_role.terraform_access.name
   policy = data.aws_iam_policy_document.tf_config_admin[count.index].json
