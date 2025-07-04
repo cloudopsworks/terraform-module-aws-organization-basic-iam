d2bb4d178a22d8070847dd7a2be4f6778133fa3a
Cristian Beraha <cristian@cloudops.works>
2025-07-04 16:29:48 -0300

    Add Access Analyzer policy support to Terraform role module
    Introduced a new policy document (`tf_access_analyzer`) to manage Access Analyzer actions. Updated `merged-policies.tf` to include this policy dynamically via the `access_analyzer` flag in `var.settings`. Renamed the AWS Backup policy to AWS Config policy for better clarity. Removed commented-out IAM role policy configurations from `policy-config.tf`.

--- modules/terraform-role/policy-config.tf
+++ modules/terraform-role/policy-config.tf
@@ -1,4 +1,4 @@
-# AWS Backup Policy
+# AWS Config Policy
 data "aws_iam_policy_document" "tf_config_admin" {
   count   = try(var.settings.awsconfig, false) ? 1 : 0
   version = "2012-10-17"
@@ -13,11 +13,3 @@ data "aws_iam_policy_document" "tf_config_admin" {
     ]
   }
 }
-#
-# resource "aws_iam_role_policy" "terraform_access_config_admin" {
-#   count  = try(var.settings.awsconfig, false) ? 1 : 0
-#   name   = "ConfigAdmin"
-#   role   = aws_iam_role.terraform_access.name
-#   policy = data.aws_iam_policy_document.tf_config_admin[count.index].json
-# }
-#
