4ca7e936d788f2321199d44bad34662c93d9f27e
Cristian Beraha <cristian@cloudops.works>
2026-04-22 12:59:58 -0300

    chore: add Step Functions admin policy to terraform-role module
    Introduced a new IAM policy document (`policy-sfn.tf`) to enable administrative access for Step Functions. The policy conditionally applies based on `settings.step_functions`-related variables and grants permissions for Step Functions (`states:*`), Lambda (`lambda:*`), and role passing (`iam:PassRole`). Integrated the policy into the module with a new IAM role policy resource.

---
+++ modules/terraform-role/policy-sfn.tf
@@ -0,0 +1,35 @@
+# Lambda Admin Policy
+data "aws_iam_policy_document" "tf_sfn_admin" {
+  count   = try(var.settings.step_functions, false) || try(var.settings.sfn, false) || try(var.settings.states, false) ? 1 : 0
+  version = "2012-10-17"
+
+  statement {
+    sid    = "StepFunctionsAccess"
+    effect = "Allow"
+    actions = [
+      "iam:PassRole",
+      "states:*"
+    ]
+    resources = concat([
+      "arn:aws:states:*:${var.account_id}:stateMachine:*"
+      ],
+      var.allowed_pass_roles
+    )
+  }
+
+  statement {
+    sid     = "StepFunctionsFullAccess"
+    effect  = "Allow"
+    actions = ["lambda:*"]
+    resources = [
+      "arn:aws:states:*:${var.account_id}:stateMachine:*"
+    ]
+  }
+}
+
+resource "aws_iam_role_policy" "terraform_access_sfn_admin" {
+  count  = try(var.settings.lambda, false) ? 1 : 0
+  name   = "StepFunctionsAdminAccess"
+  role   = aws_iam_role.terraform_access.name
+  policy = data.aws_iam_policy_document.tf_sfn_admin[count.index].json
+}
